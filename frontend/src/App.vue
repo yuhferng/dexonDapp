52ac083afd8ef9d64bca89a2aac69e58621e8b42
Jack Tseng <jacktseng@Jackde-MacBook-Air.local>
2019-01-27 11:24:27 +0800

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <appname/>
+    <gallery/>
+    <appbutton/>
+  </div>
+</template>
+
+<script>
+import appname from './components/appname'
+import gallery from './components/gallery'
+import appbutton from './components/appbutton'
+
+export default {
+  name: 'App',
+  components: {
+    appname,
+    gallery,
+    appbutton,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/gallery.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="gallery-whole">
+        <ul>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'gallery'
+}
+</script>
+
+<style scoped>
+ul {
+    display: inline-block;
+    color: aliceblue;
+    width: 58vw;
+    height:90vh;
+    background:white;
+    margin: 4.5vh 12.5vw;
+    border-color: black;
+    border-radius: 3.6vh;
+    overflow: auto;
+    text-align: center;
+}
+</style>
