ae6153d03ed34df382c965b2f214cabc495611cb
Jack Tseng <jacktseng@Jackde-MacBook-Air.local>
2019-02-17 12:38:48 +0800

    Finish shop's popup

---
+++ index.js
@@ -0,0 +1,9 @@
+function open_shop(){
+    var popup = document.getElementById('popups-shop');
+    popup.style.visibility = 'visible';
+}
+
+function close_shop(){
+    var popup = document.getElementById('popups-shop');
+    popup.style.visibility = 'hidden';
+}
