7a3cc8a562cd7f8d0762f48ed18a5f41bf21e000
Jack Tseng <jacktseng@Jackde-MacBook-Air.local>
2019-02-17 13:29:32 +0800

    finish right popups

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ function open_shop(){
     popup.style.visibility = 'visible';
 }
 
+function open_balance(){
+    var popup = document.getElementById('popups-balance');
+    popup.style.visibility = 'visible';
+}
+
 function close_shop(){
     var popup = document.getElementById('popups-shop');
+    popup.style.visibility = 'hidden';
+}
+
+function close_balance(){
+    var popup = document.getElementById('popups-balance');
     popup.style.visibility = 'hidden';
 }
