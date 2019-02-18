ceb6f47c9332805288db42829127e0a6dc9666b7
Jack Tseng <jacktseng@Jackde-MacBook-Air.local>
2019-02-18 12:07:08 +0800

    90% complete

--- app/index.js
+++ app/index.js
@@ -17,3 +17,29 @@ function close_balance(){
     var popup = document.getElementById('popups-balance');
     popup.style.visibility = 'hidden';
 }
+
+function goBackYard(){
+    var mainleft = document.getElementById('main-left');
+    var mainright = document.getElementById('main-right');
+    mainleft.style.visibility = 'hidden';
+    mainright.style.visibility = 'hidden';
+    var backyard = document.getElementById('Backyard');
+    backyard.style.visibility = 'visible';
+}
+
+function gohome(){
+    var mainleft = document.getElementById('main-left');
+    var mainright = document.getElementById('main-right');
+    mainleft.style.visibility = 'visible';
+    mainright.style.visibility = 'visible';
+    var backyard = document.getElementById('Backyard');
+    backyard.style.visibility = 'hidden';
+}
+let ClaimCountDown = 1000;
+function Claim(){
+    var claimbut = document.getElementById('claimhere');
+    claimbut.style.background = 'transparent';
+    claimbut.style.color = 'red';
+    claimbut.style.borderColor = 'black';
+    claimbut.textContent = ClaimCountDown;
+}
